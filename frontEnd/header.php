9c3827114c79dea7c8a7148de9453bf6146a31e0
HisokaBluee <mattias.gervilliers@etu.univ-amu.fr>
2022-10-08 15:45:10 +0200

    DetailedEvent page nearly finished Responsive done, still need to implement correctly the steps card

--- frontEnd/header.php
+++ frontEnd/header.php
@@ -1,5 +1,5 @@
 <?php
-function start_page($title){
+function start_page($title):void{
     ?>
     <html lang="fr-FR">
     <head>
@@ -9,6 +9,7 @@ function start_page($title){
         <title> <?php echo $title;?> </title>
         <link rel="icon" type="image/x-icon" href="../assets/images/logo_orange_no_text.ico">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../assets/styles/main.css" type="text/css" />
         <link rel="stylesheet" href="../assets/styles/style.css" type="text/css"/>
     </head>
 
