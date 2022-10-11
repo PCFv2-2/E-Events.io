7136bb0a2ac7cce44258144d49781b2a9c7fa16e
GuillaumeKusiak <guillaume.kusiak@etu.univ-amu.fr>
2022-10-11 21:03:29 +0200

    Feature : finish juryVoteManager.php

---
+++ BackEnd/BackOffice/JuryVote/juryVoteManager.php
@@ -0,0 +1,23 @@
+<?php
+header('Location: ../../../FrontEnd/juryVoteManager.php?vote');
+require_once '../../../Required.php';
+
+function eventIdVote($eventId){
+    try {
+        $dbMain = new DataBase(DataBaseEnum::MAIN_WRITE);
+        $userId = 77;
+        $currentDate = date("Y-m-d");
+        $seasonDate = $dbMain->selectQueryAndFetch('SELECT DATE_START,DATE_END FROM `SEASONS` WHERE SEASON_ID = (SELECT MAX(SEASON_ID) FROM `SEASONS`)')[0];
+        $seasonId = $dbMain->selectQueryAndFetch('SELECT SEASON_ID FROM `SEASONS` WHERE SEASON_ID = (SELECT MAX(SEASON_ID) FROM `SEASONS`)')[0][0];
+
+
+        if ($currentDate >= $seasonDate[0] && $currentDate <= $seasonDate[1]){
+            if (!is_null($eventId)){
+                $dbMain->insertQueryAndFetch('INSERT INTO `JURY_VOTE` (USER_ID,SEASON_ID,EVENT_ID) VALUES (?, ?, ?)', array($userId, $seasonId, (int)$eventId[0]), 'iii');
+            }
+        }
+    } catch (Exception $e){
+        throw new RuntimeException($e);
+    }
+
+}
