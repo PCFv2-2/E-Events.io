a047c530819022ed85d352b2e6c7fba00d46aec7
Marion Valls <marion.valls@etu.univ-amu.fr>
2022-10-12 23:39:45 +0200

    Fix

--- FrontEnd/homepage.php
+++ FrontEnd/homepage.php
@@ -9,12 +9,20 @@ startPage("Accueil",["carousel","detailedEvent"],["./Assets/Javascript/carousel"
 $dbMain = new DataBase(DataBaseEnum::MAIN_WRITE);
 //get the current season
 $data = $dbMain->selectQueryAndFetch("SELECT * FROM `SEASONS` WHERE `DATE_START`<= NOW() AND NOW() <= `DATE_END`");
-$isSeason = !(count($data)==0);
 
+
+$isSeason = !(count($data)==0);
+$idSeason=-1;
+$idSeasonPrevious=-1;
 if($isSeason){
     $idSeason=$data[0][0];
-    $data = $dbMain->selectQueryAndFetch("SELECT DISTINCT E.EVENT_ID, E.EVENT_NAME, I.IMAGE_PATH FROM `EVENTS` AS E JOIN `EVENTS_IMAGES` AS I ON E.EVENT_ID=I.EVENT_ID WHERE `SEASON_ID`<= ? ",array($idSeason),"i");
+    $idSeasonPrevious = $idSeason-1;
+    $data = $dbMain->selectQueryAndFetch("SELECT DISTINCT E.EVENT_ID, E.EVENT_NAME, I.IMAGE_PATH FROM `EVENTS` AS E JOIN `EVENTS_IMAGES` AS I ON E.EVENT_ID=I.EVENT_ID WHERE `SEASON_ID`= ? ",array($idSeason),"i");
 
+}
+else{
+    $dataPrevious = $dbMain->selectQueryAndFetch("SELECT MAX(SEASON_ID) FROM `SEASONS`");
+    $idSeasonPrevious = $dataPrevious[0][0];
 }
 
 ?>
@@ -53,24 +61,48 @@ if($isSeason){
             }
         ?>
         <br>
+        <?php
+            if($idSeasonPrevious > -1){
+                $dataPrevious = $dbMain->selectQueryAndFetch("SELECT EVENT_ID FROM `EVENTS` WHERE SEASON_ID=?",array($idSeasonPrevious),"i");
+                $best=array(array(0,0),array(0,0),array(0,0));
+                $img=array("","");
+                foreach ($dataPrevious as $key) {
+                    $count = $dbMain->selectQueryAndFetch("SELECT COUNT(*) FROM `JURY_VOTE` WHERE EVENT_ID=?", array($key[0]), "i")[0][0];
+                    for ($i = 0; $i < 3; $i++) {
+                        if ($count > $best[$i][0]) {
+                            $temp = $best[$i];
+                            $best[$i] = array($count, $key[0]);
+                            if ($i < 2) {
+                                $best[$i + 1] = $temp;
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                $dataPrevious = $dbMain->selectQueryAndFetch("SELECT IMAGE_PATH FROM `EVENTS_IMAGES` WHERE EVENT_ID=?",array($best[0][1]),"i");
+                $img[0]=$dataPrevious[0][0];
+                $dataPrevious = $dbMain->selectQueryAndFetch("SELECT IMAGE_PATH FROM `EVENTS_IMAGES` WHERE EVENT_ID=?",array($best[1][1]),"i");
+                $img[1]=$dataPrevious[0][0];
+        ?>
         <h1>Voici les évènements de la saison précédente :</h1>
         <div class="slideshow-container">
             <!-- Full-width images with number and caption text -->
             <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
-                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" style="width:100%">
+                <img src=<?php echo "\"".$img[0]."\""; ?> style="width:100%">
                 <div class="text">Caption Text</div>
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">2 / 3</div>
-                <img src="https://thumbs.dreamstime.com/b/belle-for%C3%AAt-tropicale-%C3%A0-l-itin%C3%A9raire-am%C3%A9nag%C3%A9-pour-amateurs-de-la-nature-de-ka-d-ang-36703721.jpg" style="width:100%">
+                <img src=<?php echo "\"".$img[1]."\""; ?> style="width:100%">
                 <div class="text">Caption Two</div>
             </div>
 
             <div class="mySlides fade">
                 <div class="numbertext">3 / 3</div>
-                <img src="https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320" style="width:100%">
+                <img src="https://images.assetsdelivery.com/compings_v2/tartila/tartila1904/tartila190400116.jpg" style="width:100%">
                 <div class="text">Caption Three</div>
             </div>
 
@@ -79,6 +111,9 @@ if($isSeason){
             <a href="#" class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
         <br>
+        <?php
+            }
+        ?>
 
         <!-- The dots/circles -->
         <div style="text-align:center">
